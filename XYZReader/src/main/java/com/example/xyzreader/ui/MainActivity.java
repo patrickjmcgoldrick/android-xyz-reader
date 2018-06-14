57eaf60bee08d1c8a52b1e68f66e9df2469ad117
dirtbag <pmcgoldrick@gmail.com>
2018-06-13 23:59:31 -0700

    feat: Created new MainActivity with expanding and shrinking icon

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.xyzreader.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.xyzreader.R;
+
+/**
+ * Adapted from: http://www.devexchanges.info/2016/03/android-tip-custom-coordinatorlayout.html
+ */
+
+public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
+
+    private boolean mIsTheTitleVisible          = false;
+    private boolean mIsTheTitleContainerVisible = true;
+
+    private AppBarLayout appbar;
+    private CollapsingToolbarLayout collapsing;
+    private ImageView coverImage;
+    private FrameLayout framelayoutTitle;
+    private LinearLayout linearlayoutTitle;
+    private Toolbar toolbar;
+    private TextView textviewTitle;
+    private ImageView avatar;
+
+    /**
+     * Find the Views in the layout
+     * Auto-created on 2016-03-03 11:32:38 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        appbar = (AppBarLayout)findViewById( R.id.appbar );
+        collapsing = (CollapsingToolbarLayout)findViewById( R.id.collapsing );
+        coverImage = (ImageView)findViewById( R.id.imageview_placeholder );
+        framelayoutTitle = (FrameLayout)findViewById( R.id.framelayout_title );
+        linearlayoutTitle = (LinearLayout)findViewById( R.id.linearlayout_title );
+        toolbar = (Toolbar)findViewById( R.id.toolbar );
+        textviewTitle = (TextView)findViewById( R.id.textview_title );
+        avatar = (ImageView)findViewById(R.id.avatar);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Fresco.initialize(this);
+        setContentView(R.layout.activity_main);
+        findViews();
+
+        toolbar.setTitle("");
+        appbar.addOnOffsetChangedListener(this);
+
+        setSupportActionBar(toolbar);
+        startAlphaAnimation(textviewTitle, 0, View.INVISIBLE);
+
+        //set avatar and cover
+        avatar.setImageResource(R.mipmap.ic_launcher);//setImageURI(imageUri);
+        //TODO: where does this go?  coverImage.setImageResource(R.drawable.cover);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(offset) / (float) maxScroll;
+
+        handleAlphaOnTitle(percentage);
+        handleToolbarTitleVisibility(percentage);
+    }
+
+    private void handleToolbarTitleVisibility(float percentage) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if(!mIsTheTitleVisible) {
+                startAlphaAnimation(textviewTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
+            }
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(textviewTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleVisible = false;
+            }
+        }
+    }
+
+    private void handleAlphaOnTitle(float percentage) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if(mIsTheTitleContainerVisible) {
+                startAlphaAnimation(linearlayoutTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleContainerVisible = false;
+            }
+
+        } else {
+
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(linearlayoutTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleContainerVisible = true;
+            }
+        }
+    }
+
+    public static void startAlphaAnimation (View v, long duration, int visibility) {
+        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
+
+        alphaAnimation.setDuration(duration);
+        alphaAnimation.setFillAfter(true);
+        v.startAnimation(alphaAnimation);
+    }
+}
